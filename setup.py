cbf526545007ca4c8c5a1be0609c251e683ae102
Tyler Goodlet <tgoodlet@gmail.com>
2016-08-02 17:19:23 -0400

    Package first release for pypi
    Include the headers with the source distribution so that we can build
    from pypi.

--- setup.py
+++ setup.py
@@ -1,22 +1,28 @@
 from setuptools import setup, Extension
 
 
-setup(name='ESL',
-      version='1.5.14',
-      author='FreeSWITCH Developers',
-      description='FreeSWITCH ESL module',
-      ext_modules=[
-          Extension('_ESL',
-                    sources=['esl.c',
-                             'esl_buffer.c',
-                             'esl_config.c',
-                             'esl_event.c',
-                             'esl_json.c',
-                             'esl_threadmutex.c',
-                             'esl_oop.cpp',
-                             'ESL.i'],
-                    swig_opts=['-classic', '-c++', '-DMULTIPLICITY',
-                               '-threads', '-I.'],
-                    extra_compile_args=['-I.'])
-      ],
-      py_modules=["ESL"])
+setup(
+    name='python-ESL',
+    version='1.4.18',
+    author='FreeSWITCH Developers',
+    description='FreeSWITCH Event Socket Library for Python',
+    url='https://github.com/sangoma/python-ESL',
+    download_url='https://github.com/sangoma/python-ESL/tarball/1.4.18',
+    ext_modules=[
+        Extension(
+            '_ESL',
+            sources=['esl.c',
+                     'esl_buffer.c',
+                     'esl_config.c',
+                     'esl_event.c',
+                     'esl_json.c',
+                     'esl_threadmutex.c',
+                     'esl_oop.cpp',
+                     'ESL.i'],
+            swig_opts=['-classic', '-c++', '-DMULTIPLICITY',
+                       '-threads', '-I.'],
+            extra_compile_args=['-I.']
+        )
+    ],
+    py_modules=["ESL"]
+)
